5e92748b2ebb632223ae5d94ce3bf020a30a18d1
petiap <petiap@yahoo.com>
2014-07-13 17:22:46 +0300

    Initial commit of the project

---
+++ Hangman/TopPlayer.cs
@@ -0,0 +1,22 @@
+﻿namespace Hangman
+{
+    using System;
+
+    class TopPlayer
+    {
+        private string playerName;
+        private double playerScore;
+
+        public string PlayerName
+        {
+            get { return this.playerName; }
+            set { this.playerName = value; }
+        }
+
+        public double PlayerScore
+        {
+            get { return this.playerScore; }
+            set { this.playerScore = value; }
+        }
+    }
+}
