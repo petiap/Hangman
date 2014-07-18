87badc4df7e6164a54ff1e252ce40c9b04cad80e
petiap <petiap@yahoo.com>
2014-07-19 00:42:17 +0300

    Implements a State Behavioral Pattern
    Adds actor classes for the pattern:
    Context -> Game
    State -> GameState
    Concrete State -> InitialState, PlayState
    
    Modifies Hangman to reflect the implemented pattern
    
    Code clean-up

---
+++ Hangman/Game.cs
@@ -0,0 +1,28 @@
+﻿namespace Hangman
+{
+    using System;
+
+    public class Game
+    {
+        private GameState state;
+        private Word word;
+        private int numberOfMistakes;
+
+        public Game(GameState gameState)
+        {
+            this.State = gameState;
+        }
+
+        // TODO: Implement value checks
+        public GameState State { get; set; }
+
+        public Word Word { get; set; }
+
+        public int NumberOfMistakes { get; set; }
+
+        public void Run()
+        {
+            this.State.PerformAction(this);
+        }
+    }
+}
---
+++ Hangman/PlayState.cs
@@ -0,0 +1,36 @@
+﻿namespace Hangman
+{
+    using System;
+    using System.Linq;
+
+    public class PlayState : GameState
+    {
+        public override void PerformAction(Game game)
+        {
+            if (game.Word.MaskedWord.Contains('_'))
+            {
+                Console.WriteLine("The secret word is " + game.Word.MaskedWord);
+
+                Console.Write("Enter your guess: ");
+
+                string command = Console.ReadLine();
+
+                if (command.Length == 1)
+                {
+                    char letter = command[0];
+
+                    GameEngine.GuessLetter(game.Word, letter, game.NumberOfMistakes);
+                }
+                else
+                {
+                    CommandInterpreter.Decode(command);
+                }
+            }
+            else
+            {
+                GameEngine.EstimateScore(game.Word, game.NumberOfMistakes);
+                game.NumberOfMistakes = 0;
+            }
+        } 
+    }
+}
