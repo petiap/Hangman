d6af498087ebb722cd94c6a714574d1e8493938f
petiap <petiap@yahoo.com>
2014-07-19 00:37:55 +0300

    Adds new class GameEngine - wrapper for all main game methods

---
+++ Hangman/GameEngine.cs
@@ -0,0 +1,78 @@
+﻿namespace Hangman
+{
+    using System;
+
+    public static class GameEngine
+    {
+        private static bool notUseHelp = true;
+
+        public static void GuessLetter(Word secretWord, char letter, int numberOfMistakes)
+        {
+            if (secretWord.IsLetter(letter) && secretWord.ContainsLetter(letter))
+            {
+                secretWord.RevealLetterPosition(letter);
+
+                int numberOfOccurrences = secretWord.NumberOfLetterOccurrences(letter);
+                string comment = numberOfOccurrences == 1 ? " letter" : " letters";
+
+                Console.WriteLine("Good job! You revealed " + numberOfOccurrences + comment);
+            }
+            else
+            {
+                Console.WriteLine("Sorry! There are no unrevealed letters \"{0}\"", char.ToLower(letter));
+                numberOfMistakes++;
+            }
+        }
+
+        public static void GiveHint(Word secretWord)
+        {
+            char hintLetter = secretWord.MaskedWord[secretWord.MaskedWord.IndexOf('_') / 2];
+
+            Console.WriteLine("OK, I reveal for you the next letter \'{0}\'", hintLetter);
+
+            int firstMissingLetter = secretWord.MaskedWord.IndexOf('_');
+
+            secretWord.RevealLetterPosition(secretWord.MaskedWord[firstMissingLetter / 2]);
+
+            notUseHelp = false;
+        }
+
+        public static void EstimateScore(Word secretWord, int numberOfMistakes)
+        {
+            bool isNewTopScore = Scoreboard.ScoreboardInstance.IsNewTopScore(numberOfMistakes);
+            string comment = numberOfMistakes == 1 ? " mistake" : " mistakes";
+
+            Console.WriteLine("The secret word is " + secretWord.MaskedWord);
+            Console.Write("\nYou won with " + numberOfMistakes + comment);
+
+            if (notUseHelp && isNewTopScore)
+            {
+                Console.Write("\nPlease enter your name for the top scoreboard: ");
+
+                string newTopPlayerName = Console.ReadLine();
+                Player newTopPlayer = new Player(newTopPlayerName, numberOfMistakes);
+
+                Scoreboard.ScoreboardInstance.AddPlayer(newTopPlayer);
+
+                GameEngine.ShowScoreboard();
+            }
+            else if (!isNewTopScore)
+            {
+                Console.Write(" but your result is lower than top scores\n");
+            }
+            else
+            {
+                Console.Write(" but you have cheated. You are not allowed to enter into the scoreboard.\n");
+            }
+
+            notUseHelp = true;
+        }
+
+        public static void ShowScoreboard()
+        {
+            Console.WriteLine("Scoreboard:");
+
+            Console.WriteLine(Scoreboard.ScoreboardInstance.ToString());
+        }
+    }
+}
