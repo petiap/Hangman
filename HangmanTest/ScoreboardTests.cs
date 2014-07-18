760648f6722ea44546293e4ad8a987f326177d59
petiap <petiap@yahoo.com>
2014-07-18 04:21:17 +0300

    Adds Moq library to the project and initial Scoreboard unit tests

---
+++ HangmanTest/ScoreboardTests.cs
@@ -0,0 +1,35 @@
+﻿namespace HangmanTest
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Hangman;
+    using Moq;
+
+    [TestClass]
+    public class ScoreboardTests
+    {
+        [TestMethod]
+        public void VerifySCoreboardSingletonInstanceIsCorrect()
+        {
+            var scoreboardMock = new Mock<Scoreboard>();
+        }
+
+        [TestMethod]
+        public void VerifyThatAddPlayerWorksCorrectly()
+        {
+            string testPlayerName = "Rinswind";
+            int testPlayerScore = 12345;
+            Player testPlayer = new Player(testPlayerName, testPlayerScore);
+
+            Scoreboard.ScoreboardInstance.AddPlayer(testPlayer);
+
+            PrivateObject po = new PrivateObject(typeof(Scoreboard));
+            var players = (List<Player>)po.GetField("topPlayers");
+            Player referencePlayer = players[0];
+
+            Assert.AreEqual(testPlayer, referencePlayer);
+        }
+    }
+}
