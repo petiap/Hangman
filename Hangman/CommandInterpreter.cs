6ce1497709390308eb60be61d4de81326a807f85
petiap <petiap@yahoo.com>
2014-07-19 00:39:43 +0300

    Adds new class CommandInterpreter
    Comprises of a delegate extracting the game variable and a method for decoding the game commands

---
+++ Hangman/CommandInterpreter.cs
@@ -0,0 +1,44 @@
+﻿namespace Hangman
+{
+    using System;
+
+    public delegate Game GetGameDelegate();
+
+    public static class CommandInterpreter
+    {
+        private static GetGameDelegate getGame;
+
+        public static void AssignGameDelegate(GetGameDelegate getGameDelegate)
+        {
+            getGame = getGameDelegate;
+        }
+
+        public static void Decode(string command)
+        {
+            switch (command)
+            {
+                case "help":
+                    GameEngine.GiveHint(getGame().Word);
+                    break;
+
+                case "exit":
+                    Environment.Exit(0);
+                    break;
+
+                case "restart":
+                    getGame().State = new InitialState();
+                    Console.WriteLine();
+                    break;
+
+                case "top":
+                    GameEngine.ShowScoreboard();
+                    break;
+
+                default:
+                    Console.WriteLine("Incorect guess or command!");
+                    getGame().NumberOfMistakes++;
+                    break;
+            }
+        }
+    }
+}
