8e9e45d6dd3bc13599a762daf87bf61dac13716d
petiap <petiap@yahoo.com>
2014-07-18 04:22:09 +0300

    Adds Word unit tests file

---
+++ HangmanTest/WordTests.cs
@@ -0,0 +1,14 @@
+﻿namespace HangmanTest
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class WordTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
