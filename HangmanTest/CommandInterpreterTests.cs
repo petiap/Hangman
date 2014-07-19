5e3907b4380f853378dcd9017931fe16e958cb93
RosenTodorov <kobe_8@abv.bg>
2014-07-19 23:44:12 +0300

    new tests

---
+++ HangmanTest/CommandInterpreterTests.cs
@@ -0,0 +1,18 @@
+﻿namespace HangmanTest
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Hangman;
+
+    [TestClass]
+    public class CommandInterpreterTests
+    {
+        [TestMethod]
+        public void DecodeCommandTests()
+        {
+            string exampleWord = "computer";
+
+            CommandInterpreter decodeCommand = new CommandInterpreter
+        }
+    }
+}
