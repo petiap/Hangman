c77d3fa272def1c1d6f504387699ea6615823a84
ivan-yosifov <ivan_yosifov@yahoo.com>
2014-07-20 22:23:13 +0300

    Added new lines and better formatting of getters

--- Hangman/Game.cs
+++ Hangman/Game.cs
@@ -1,10 +1,10 @@
 ﻿namespace Hangman
 {
     using System;
-	
+
     public class Game
     {
-		private const int MaxPosibleMistakes=28;
+        private const int MaxPosibleMistakes = 28;
 
         private GameState state;
         private Word word;
@@ -15,66 +15,80 @@
             this.State = gameState;
         }
 
-        public GameState State 
-		{ 
-			get { return this.state; }
-	        set
-	        {
-				if (string.IsNullOrEmpty(value.ToString()))
-				{
-					throw new ArgumentNullException("Value of the state is null or missing.");
-				}
+        public GameState State
+        {
+            get 
+            {
+                return this.state; 
+            }
 
-		        this.state = value;
-	        }
-		}
+            set
+            {
+                if (string.IsNullOrEmpty(value.ToString()))
+                {
+                    throw new ArgumentNullException("Value of the state is null or missing.");
+                }
 
-	    public Word Word
-	    {
-		    get { return this.word; }
-		    set
-		    {
-				if (string.IsNullOrEmpty(value.ToString()))
-				{
-					throw new ArgumentNullException("Value of the word is null or missing.");
-				}
+                this.state = value;
+            }
+        }
 
-			    if (string.IsNullOrEmpty(value.MaskedWord))
-			    {
-					throw new ArgumentNullException("Value of the masked word is null or missing.");
-			    }
+        public Word Word
+        {
+            get 
+            {
+                return this.word; 
+            }
 
-				if (string.IsNullOrEmpty(value.SecretWord))
-				{
-					throw new ArgumentNullException("Value of the secret word is null or missing.");
-				}
-				this.word = value;
-		    }
-	    }
+            set
+            {
+                if (string.IsNullOrEmpty(value.ToString()))
+                {
+                    throw new ArgumentNullException("Value of the word is null or missing.");
+                }
 
-		public int Mistakes
-		{
-			get { return this.mistakes; }
-			set
-			{
-				if (string.IsNullOrWhiteSpace(value.ToString()))
-				{
-					throw new ArgumentNullException("Value of mistakes is null or white space.");
-				}
+                if (string.IsNullOrEmpty(value.MaskedWord))
+                {
+                    throw new ArgumentNullException("Value of the masked word is null or missing.");
+                }
 
-				if (value < 0)
-				{
-					throw new ArgumentOutOfRangeException("Value of the mistakes is less than zero.");
-				}
+                if (string.IsNullOrEmpty(value.SecretWord))
+                {
+                    throw new ArgumentNullException("Value of the secret word is null or missing.");
+                }
 
-				if (value > MaxPosibleMistakes)
-				{
-					throw new ArgumentOutOfRangeException("Value of the mistakes is more" +
-						" than all letters in the alphabet.");
-				}
-				this.mistakes = value;
-			}
-		}
+                this.word = value;
+            }
+        }
+
+        public int Mistakes
+        {
+            get 
+            {
+                return this.mistakes; 
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value.ToString()))
+                {
+                    throw new ArgumentNullException("Value of mistakes is null or white space.");
+                }
+
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Value of the mistakes is less than zero.");
+                }
+
+                if (value > MaxPosibleMistakes)
+                {
+                    throw new ArgumentOutOfRangeException("Value of the mistakes is more" +
+                        " than all letters in the alphabet.");
+                }
+
+                this.mistakes = value;
+            }
+        }
 
         public void Run()
         {
