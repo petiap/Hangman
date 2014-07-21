8bb6829f8ab911601d477df52cc57f0c244e00fd
ppetrova <petiap@yahoo.com>
2014-07-21 09:24:43 +0300

    Updates Player

--- Hangman/Player.cs
+++ Hangman/Player.cs
@@ -1,7 +1,6 @@
 ﻿namespace Hangman
 {
     using System;
-    using System.Text;
 
     public class Player
     {
@@ -23,17 +22,10 @@
 
             set
             {
-				decimal number;
-
-				if (string.IsNullOrWhiteSpace(value))
-				{
-					throw new ArgumentNullException("Value of top player name is null or white space.");
-				}
-
-				if (decimal.TryParse(value, out number))
-				{
-					throw new ArgumentOutOfRangeException("Value of top player name is a number.");
-				}
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Name", "Value can't be null or empty string");
+                }
 
 				this.name = value;
             }
@@ -48,35 +40,13 @@
 
             set
             {
-				if (string.IsNullOrWhiteSpace(value.ToString()))
-				{
-					throw new ArgumentNullException("Value of word to guess is null or white space.");
-				}
-
 				if (value < 0)
 				{
-					throw new ArgumentOutOfRangeException("Value of the player score is less than zero.");
-				}
-
-				if (value > 28)
-				{
-					throw new ArgumentOutOfRangeException("Value of the player score is more" +
-						" than all letters in the alphabet.");
+					throw new ArgumentNullException("Score", "Value can't be negative");
 				}
 
 				this.score = value;
             }
         }
-
-		// it will be better to have it here and call it in scoreboard directly
-	    public override string ToString()
-	    {
-		    StringBuilder str = new StringBuilder();
-		    string comment = this.Score == 1 ? "mistake" : "mistakes";
-
-		    str.Append(this.Name + " --> " + this.Score + " " + comment);
-
-		    return str.ToString();
-	    }
     }
 }
--- HangmanTest/PlayerTests.cs
+++ HangmanTest/PlayerTests.cs
@@ -1,8 +1,9 @@
 ﻿namespace HangmanTest
 {
     using System;
-    using Hangman;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Hangman;
 
     [TestClass]
     public class PlayerTests
@@ -54,85 +55,5 @@
 
             testPlayer = new Player(testPlayerName, testPlayerScore);
         }
-
-		public void TopPlayerCreation()
-		{
-			string name = "New guy";
-			int score = 5;
-
-			Player topPlayer = new Player(name, score);
-
-			Assert.IsNotNull(topPlayer);
-			Assert.AreEqual(name, topPlayer.Name);
-			Assert.AreEqual(score, topPlayer.Score);
-		}
-
-		[TestMethod]
-		[ExpectedException(typeof(ArgumentNullException))]
-		public void TopPlayerNameAsWhitespace()
-		{
-			string name = " ";
-			int score = 6;
-
-			Player topPlayer = new Player(name, score);
-		}
-
-		[TestMethod]
-		[ExpectedException(typeof(ArgumentNullException))]
-		public void TopPlayerNameAsEmptyString()
-		{
-            string name = string.Empty;
-			int score = 5;
-
-			Player topPlayer = new Player(name, score);
-		}
-
-		[TestMethod]
-		[ExpectedException(typeof(ArgumentOutOfRangeException))]
-		public void TopPlayerNameAsNumber()
-		{
-			string name = "213432.34532";
-			int score = 9;
-
-			Player topPlayer = new Player(name, score);
-		}
-
-		[TestMethod]
-		[ExpectedException(typeof(ArgumentOutOfRangeException))]
-		public void TopPlayerScoreLessThanZero()
-		{
-			string name = "New Guy";
-			int score = -1;
-
-			Player topPlayer = new Player(name, score);
-		}
-
-		[TestMethod]
-		[ExpectedException(typeof(ArgumentOutOfRangeException))]
-		public void TopPlayerScoreIsMoreThanAllLettersInAlphabeth()
-		{
-			string name = "New Guy";
-			int score = 30;
-
-			Player topPlayer = new Player(name, score);
-		}
-
-		[TestMethod]
-		[ExpectedException(typeof(ArgumentNullException))]
-		public void TopPlayerScoreAsNull()
-		{
-			string name = "New Guy";
-
-			Player topPlayer = new Player(name, int.Parse(null));
-		}
-
-		[TestMethod]
-		[ExpectedException(typeof(FormatException))]
-		public void TopPlayerScoreAsEmptyString()
-		{
-			string name = "New Guy";
-
-			Player topPlayer = new Player(name, int.Parse(string.Empty));
-		}
     }
 }
