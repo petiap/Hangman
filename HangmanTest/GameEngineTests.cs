b80953363e606cff86ac17cb0d646c632b12d231
RosenTodorov <kobe_8@abv.bg>
2014-07-22 13:14:43 +0300

    new tests

---
+++ HangmanTest/GameEngineTests.cs
@@ -0,0 +1,110 @@
+﻿namespace HangmanTest
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Hangman;
+
+    [TestClass]
+    public class GameEngineTest
+    {
+        [TestMethod]
+        public void GameEngineContainsLetter()
+        {
+            char letter = 'd';
+
+            Game game = new Game(new InitialState());
+
+            GameEngine.GuessLetter(game, letter);
+        }
+
+        [TestMethod]
+        public void GameEngineNotContainsLetter()
+        {
+            char letter = 'q';
+
+            Game game = new Game(new InitialState());
+
+            GameEngine.GuessLetter(game, letter);
+        }
+
+        [TestMethod]
+        public void TheLetterAsNumber()
+        {
+            char letter = '7';
+
+            Game game = new Game(new InitialState());
+
+            GameEngine.GuessLetter(game, letter);
+        }
+
+        [TestMethod]
+        public void TheLetterAsSomeChar()
+        {
+            char letter = '%';
+
+            Game game = new Game(new InitialState());
+
+            GameEngine.GuessLetter(game, letter);
+        }
+
+        [TestMethod]
+        public void GameEngineIsLetterAsLetter()
+        {
+            char letter = 't';
+
+            Game game = new Game(new InitialState());
+
+            GameEngine.GuessLetter(game, letter); ;
+        }
+
+        [TestMethod]
+        public void WordIsLetterAsNonLetter()
+        {
+            char letter = '$';
+
+            Game game = new Game(new InitialState());
+
+            GameEngine.GuessLetter(game, letter);
+        }
+
+        [TestMethod]
+        public void GameState()
+        {
+            char letter = '$';
+
+            Game game = new Game(new InitialState());
+
+            GameEngine.GuessLetter(game, letter);
+        }
+
+        [TestMethod]
+        public void GameEngineGiveHintTest()
+        {
+            Word word = new Word();
+
+            GameEngine.GiveHint(word);
+        }
+
+        [TestMethod]
+        public void GameEngineEstimateScoreTest()
+        {
+            Word word = new Word();
+
+            GameEngine.EstimateScore(word, 1);
+        }
+
+        [TestMethod]
+        public void GameEngineEstimateScoreWithHelpTest()
+        {
+            Word word = new Word();
+
+            GameEngine.GiveHint(word);
+            GameEngine.EstimateScore(word, 1);
+        }
+
+        [TestMethod]
+        public void GameEngineShowScoreboardTest()
+        {
+            GameEngine.ShowScoreboard();
+        }    
+    }
+}
