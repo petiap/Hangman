2c9107b9a905a257ad10d732d24c8cce59421180
RosenTodorov <kobe_8@abv.bg>
2014-07-22 13:15:27 +0300

    new tests

--- HangmanTest/PlayerTests.cs
+++ HangmanTest/PlayerTests.cs
@@ -55,5 +55,85 @@
 
             testPlayer = new Player(testPlayerName, testPlayerScore);
         }
+
+        public void TopPlayerCreation()
+        {
+            string name = "New player";
+            int topScore = 5;
+
+            Player topPlayer = new Player(name, topScore);
+
+            Assert.IsNotNull(topPlayer);
+            Assert.AreEqual(name, topPlayer.Name);
+            Assert.AreEqual(topScore, topPlayer.Score);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TopPlayerNameAsEmptyString()
+        {
+            string name = string.Empty;
+            int topScore = 5;
+
+            Player topPlayer = new Player(name, topScore);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TopPlayerNameAsWhitespace()
+        {
+            string name = " ";
+            int topScore = 5;
+
+            Player topPlayer = new Player(name, topScore);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TopPlayerScoreLessThanZero()
+        {
+            string name = "New player";
+            int topScore = -1;
+
+            Player topPlayer = new Player(name, topScore);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TopPlayerNameAsNumber()
+        {
+            string name = "4526.34561";
+            int topScore = 5;
+
+            Player topPlayer = new Player(name, topScore);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TopPlayerScoreIsMoreThanAllLettersInAlphabeth()
+        {
+            string name = "New player";
+            int topScore = 30;
+
+            Player topPlayer = new Player(name, topScore);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void TopPlayerScoreAsEmptyString()
+        {
+            string name = "New player";
+
+            Player topPlayer = new Player(name, int.Parse(string.Empty));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TopPlayerScoreAsNull()
+        {
+            string name = "New player";
+
+            Player topPlayer = new Player(name, int.Parse(null));
+        }
     }
 }
